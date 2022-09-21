79a1189490357f18985f706934dad5e49492c052
Michel Marinho <michel.marinho04@outlook.com>
2022-09-21 18:26:18 -0400

    Add: Método DELETE

--- src/app.js
+++ src/app.js
@@ -39,6 +39,13 @@ app.put('/livros/:id', (req, res) => {
   res.json(livros)
 })
 
+app.delete('/livros/:id', (req, res) => {
+  let { id } = req.params
+  let index = buscaLivro(id)
+  livros.splice(index, 1)
+  res.send(`Livro ${id} removido com sucesso `)
+})
+
 function buscaLivro(id) {
   return livros.findIndex((livro) => livro.id == id)
 }
