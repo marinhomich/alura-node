220211d450a4a7bb8295a29acd5de638c30351a2
Michel Marinho <michel.marinho04@outlook.com>
2022-09-22 17:34:51 -0400

    Add: Livro Controller

---
+++ src/controllers/livrosController.js
@@ -0,0 +1,11 @@
+import livros from '../models/Livro.js'
+
+class livroController {
+  static listarLivros = (req, res) => {
+    livros.find((err, livros) => {
+      res.status(200).json(livros)
+    })
+  }
+}
+
+export default livroController
